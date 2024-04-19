7e50853e90acf95cf5343518cc2c4f05fa9d74ca
theweeknd982 <theweeknd982@gmail.com>
2024-04-19 13:05:11 +0900

    9205 bfs

--- DFS&BFS/9205/app.py
+++ DFS&BFS/9205/app.py
@@ -1,35 +1,37 @@
-def bfs(sj,si,ej,ei):
-    # q생성, 필요변수 등 생성
-    q = []
-    v = [0]*N
+from collections import deque
 
-    # 큐에 초기데이터 삽입, si,sj는 v에 표시X
-    q.append((sj,si))
+def bfs(sj, si, ej, ei):
+    # q 생성 및 필요 변수 초기화
+    q = deque()
+    v = [0] * N  # 방문 배열
+
+    # 큐에 초기 데이터 삽입, 시작지점은 방문 표시 X
+    q.append((sj, si))
     
     while q:
-        cj, ci = q.pop(0)
-        if abs(cj-ej)+abs(ci-ei)<=1000: #목적지 도달가능
+        cj, ci = q.popleft()  # deque 사용하여 O(1)로 pop
+        if abs(cj - ej) + abs(ci - ei) <= 1000:  # 목적지 도달 가능
             return 'happy'
         
-        # 미방문 모든 편의점좌표: 범위내 인지체크
+        # 미방문 모든 편의점 좌표: 범위 내인지 체크
         for i in range(N):
-            if v[i]==0: #미방문 편의점
-                nj,ni = lst[i]
-                if abs(cj-nj)+abs(ci-ni)<=1000:
-                    q.append((nj,ni))
-                    v[i]=1
+            if v[i] == 0:  # 미방문 편의점
+                nj, ni = lst[i]
+                if abs(cj - nj) + abs(ci - ni) <= 1000:
+                    q.append((nj, ni))
+                    v[i] = 1
                     
     return 'sad'        
 
 TC = int(input())
 for _ in range(TC):
-    N = int(input())
-    sj, si = map(int,input().split())
-    lst =[]
+    N = int(input())  # 편의점 수
+    sj, si = map(int, input().split())  # 시작 좌표
+    lst = []
     for _ in range(N):
-        tj, ti = map(int,input().split())
-        lst.append((ti,tj))
-    ej, ei = map(int, input().split())
+        tj, ti = map(int, input().split())
+        lst.append((tj, ti))
+    ej, ei = map(int, input().split())  # 도착 좌표
 
-    ans = bfs(sj,si,ej,ei)
+    ans = bfs(sj, si, ej, ei)
     print(ans)
