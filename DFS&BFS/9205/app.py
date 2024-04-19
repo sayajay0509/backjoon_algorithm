a55c631a81c14828af5f09c16f27e24d1ebbb3c8
theweeknd982 <theweeknd982@gmail.com>
2024-04-19 13:03:03 +0900

    9205

--- DFS&BFS/9205/app.py
+++ DFS&BFS/9205/app.py
@@ -1,3 +1,26 @@
+def bfs(sj,si,ej,ei):
+    # q생성, 필요변수 등 생성
+    q = []
+    v = [0]*N
+
+    # 큐에 초기데이터 삽입, si,sj는 v에 표시X
+    q.append((sj,si))
+    
+    while q:
+        cj, ci = q.pop(0)
+        if abs(cj-ej)+abs(ci-ei)<=1000: #목적지 도달가능
+            return 'happy'
+        
+        # 미방문 모든 편의점좌표: 범위내 인지체크
+        for i in range(N):
+            if v[i]==0: #미방문 편의점
+                nj,ni = lst[i]
+                if abs(cj-nj)+abs(ci-ni)<=1000:
+                    q.append((nj,ni))
+                    v[i]=1
+                    
+    return 'sad'        
+
 TC = int(input())
 for _ in range(TC):
     N = int(input())
@@ -7,3 +30,6 @@ for _ in range(TC):
         tj, ti = map(int,input().split())
         lst.append((ti,tj))
     ej, ei = map(int, input().split())
+
+    ans = bfs(sj,si,ej,ei)
+    print(ans)
