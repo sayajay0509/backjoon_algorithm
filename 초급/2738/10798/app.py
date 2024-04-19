51948fbd40a427c04f0212c92a7493004a64f89d
theweeknd982 <theweeknd982@gmail.com>
2024-04-19 14:57:46 +0900

    10798

--- 초급/2738/10798/app.py
+++ 초급/2738/10798/app.py
@@ -1,8 +1,8 @@
-matrix = [input() for _ in range(5)]
-ans=''
-for i in range(15)
+matrix = [input() for _ in range(5)]  # 최대 5개의 문자열 입력
+output = ''
+for i in range(15):  # 최대 15자까지 있을 수 있으므로
     for string in matrix:
-        if i < len(string):
-            ans += string[i]
+        if i < len(string):  # 해당 문자열의 길이가 i보다 큰 경우에만 접근
+            output += string[i]
 
-print(ans)
+print(output)
