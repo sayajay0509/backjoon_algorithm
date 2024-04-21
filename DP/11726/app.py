7e8083268a7f4eed43801de27c3276d631576281
theweeknd982 <theweeknd982@gmail.com>
2024-04-21 16:48:05 +0900

    11726 DP

--- DP/11726/app.py
+++ DP/11726/app.py
@@ -1,8 +1,14 @@
 N = int(input())
-dp=[0]*(N+1)
-dp[0],dp[1]=1,1
-for i in rage(2,N+1):
-    dp[i]=dp[i-1]+dp[i-2]*2
-    
-ans = dp(N)
-print(ans%10007)
+dp = [0] * (N + 1)
+
+# N이 1보다 작은 경우를 처리하기 위해 조건문 추가
+if N >= 1:
+    dp[1] = 1
+if N >= 2:
+    dp[2] = 2
+
+for i in range(3, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007  # 바로 모듈로 연산을 적용하여 오버플로우 방지
+
+ans = dp[N]
+print(ans)
