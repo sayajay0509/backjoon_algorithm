952cb0c9f21648934176e6598d935f58e5ce3720
theweeknd982 <theweeknd982@gmail.com>
2024-04-27 16:29:21 +0900

    2644 BFS

--- DFS&BFS/2644/app3.py
+++ DFS&BFS/2644/app3.py
@@ -1,28 +1,32 @@
-def bfs(s,e):
-    q= []
-    v=[0]*(N+1)
+from collections import deque
+
+def bfs(s, e):
+    q = deque()  # deque로 큐 생성
+    v = [0] * (N + 1)
     q.append(s)
-    v[s]=1
+    v[s] = 1
     while q:
-        c = q.pop(0)
+        c = q.popleft()  # deque의 popleft를 사용하여 O(1) 시간 복잡도 보장
         if c == e:
-            return v[e]-1
+            return v[e] - 1
 
         for n in adj[c]:
             if not v[n]:
                 q.append(n)
-                v[n] +=v[c]+1
+                v[n] = v[c] + 1
 
     return -1
 
-N = int(input())
-S, E = map(int,input().split())
-M = int(input())
-adj = [[] for _ in range(N+1)]
+# 입력 받는 부분과 인접 리스트 생성
+N = int(input("Enter the number of nodes: "))
+S, E = map(int, input("Enter start and end nodes separated by space: ").split())
+M = int(input("Enter the number of edges: "))
+adj = [[] for _ in range(N + 1)]
 for _ in range(M):
-    p, c = map(int,input().split())
+    p, c = map(int, input("Enter connected nodes separated by space: ").split())
     adj[p].append(c)
     adj[c].append(p)
 
-ans = bfs(S,E)
+# BFS 실행 및 결과 출력
+ans = bfs(S, E)
 print(ans)
